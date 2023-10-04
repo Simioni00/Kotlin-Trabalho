58842b3256d2eede44096ed09695c2891a5e5b4b
ricardo <dallarosizr@gmail.com>
2023-10-04 20:42:18 -0300

    commit drawer navigation

--- app/src/main/java/com/example/kotlin_trabalho/TelaInicialActivity.kt
+++ app/src/main/java/com/example/kotlin_trabalho/TelaInicialActivity.kt
@@ -3,13 +3,35 @@ package com.example.kotlin_trabalho
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class TelaInicialActivity : AppCompatActivity() {
+class TelaInicialActivity : AppCompatActivity(){
+
+    private lateinit var drawerLayout: DrawerLayout
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicialmain)
+
+
     }
+
+
+
+
+
+
+
+
 
     fun onClickProximaPagina(view: View) {
         val intent = Intent(this, TelaSecundariaActivity::class.java)
